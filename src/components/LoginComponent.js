582034beb74c58b748bd5eb6c62261775b390132
prerna-p <prernasocialid@gmail.com>
2018-12-07 15:17:08 -0500

    css improvments and installed reactstrap, fontawesome

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import { Jumbotron, Container } from 'reactstrap';
 
 export default class LoginComponent extends Component{
     constructor(props){
@@ -10,49 +11,61 @@ export default class LoginComponent extends Component{
 
     render(){
         return (
-            <div className="container">
-                <h1>
-                    Sign In
-                </h1>
-                <form>
-                    <div className="form-group row">
-                        <label htmlFor="username"
-                               className="col-sm-2 col-form-label">
-                            Username
-                        </label>
-                        <div className="col-sm-8">
-                            <input className="form-control"
-                                   id="username"
-                                   placeholder="alice"
-                                   onChange={(e) => this.username = e.target.value}/>
-                        </div>
-                    </div>
-                    <div className="form-group row">
-                        <label htmlFor="password"
-                               className="col-sm-2 col-form-label">
-                            Password
-                        </label>
-                        <div className="col-sm-8">
-                            <input type="password"
-                                   className="form-control"
-                                   id="password"
-                                   placeholder="123qwe#$%"
-                                   onChange={(e) => this.password = e.target.value}
-                                   required/>
-                        </div>
-                    </div>
+            <div>
+                <Jumbotron fluid className="bg-light">
+                    <Container>
 
-                    <div className="form-group row">
-                        <label className="col-sm-2 col-form-label"></label>
-                        <div className="col-sm-8">
-                            <button className="btn btn-primary btn-block"
-                                    type="button"
-                                    id="signInBtn">
-                                Sign in
-                            </button>
+                        <h1 className="display-3">
+                            <i className="fa fa-video-camera pr-2 text-warning"></i>
+                            Pick Flick
+                        </h1>
+                        <p className="lead text-secondary">The one stop hub for movie aficionados </p>
+                    </Container>
+                </Jumbotron>
+                <Container>
+                    <h1>
+                        Sign In
+                    </h1>
+                    <form>
+                        <div className="form-group row">
+                            <label htmlFor="username"
+                                   className="col-sm-2 col-form-label">
+                                Username
+                            </label>
+                            <div className="col-sm-8">
+                                <input className="form-control"
+                                       id="username"
+                                       placeholder="alice"
+                                       onChange={(e) => this.username = e.target.value}/>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                        <div className="form-group row">
+                            <label htmlFor="password"
+                                   className="col-sm-2 col-form-label">
+                                Password
+                            </label>
+                            <div className="col-sm-8">
+                                <input type="password"
+                                       className="form-control"
+                                       id="password"
+                                       placeholder="123qwe#$%"
+                                       onChange={(e) => this.password = e.target.value}
+                                       required/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-2 col-form-label"></label>
+                            <div className="col-sm-8">
+                                <button className="btn btn-primary btn-block"
+                                        type="button"
+                                        id="signInBtn">
+                                    Sign in
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </Container>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
 import App from "./app";
 
 
