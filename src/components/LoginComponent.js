bdf8dfb1bc603e83cb54315662d59a98de6446de
shaivalpatel <patel.shaiv@husky.neu.edu>
2018-12-08 13:43:26 -0500

    minor chnages

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -23,71 +23,71 @@ export default class LoginComponent extends Component{
 
     }
 
-    render(){
-        if(this.state.redirect === true){
-            return <Redirect
+    render() {
+        if (this.state.redirect === true) {
+            return (<Redirect
                 to={{
-                    pathname:  "/" + this.state.user._id+ "/home",
+                    pathname: "/" + this.state.user._id + "/home",
 
-                }}/>
-        return (
-            <div>
-                <Jumbotron fluid className="bg-light">
+                }}/>)}
+            return (
+                <div>
+                    <Jumbotron fluid className="bg-light">
+                        <Container>
+
+                            <h1 className="display-3">
+                                <i className="fa fa-video-camera pr-1 text-warning"></i>
+                                Pick Flick
+                            </h1>
+                            <p className="lead text-secondary">The one stop hub for movie aficionados </p>
+                        </Container>
+                    </Jumbotron>
                     <Container>
-
-                        <h1 className="display-3">
-                            <i className="fa fa-video-camera pr-1 text-warning"></i>
-                            Pick Flick
+                        <h1>
+                            Sign In
                         </h1>
-                        <p className="lead text-secondary">The one stop hub for movie aficionados </p>
-                    </Container>
-                </Jumbotron>
-                <Container>
-                    <h1>
-                        Sign In
-                    </h1>
-                    <form>
-                        <div className="form-group row">
-                            <label htmlFor="username"
-                                   className="col-sm-2 col-form-label">
-                                Username
-                            </label>
-                            <div className="col-sm-8">
-                                <input className="form-control"
-                                       id="username"
-                                       placeholder="alice"
-                                       onChange={(e) => this.username = e.target.value}/>
+                        <form>
+                            <div className="form-group row">
+                                <label htmlFor="username"
+                                       className="col-sm-2 col-form-label">
+                                    Username
+                                </label>
+                                <div className="col-sm-8">
+                                    <input className="form-control"
+                                           id="username"
+                                           placeholder="alice"
+                                           onChange={(e) => this.username = e.target.value}/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="form-group row">
-                            <label htmlFor="password"
-                                   className="col-sm-2 col-form-label">
-                                Password
-                            </label>
-                            <div className="col-sm-8">
-                                <input type="password"
-                                       className="form-control"
-                                       id="password"
-                                       placeholder="123qwe#$%"
-                                       onChange={(e) => this.password = e.target.value}
-                                       required/>
+                            <div className="form-group row">
+                                <label htmlFor="password"
+                                       className="col-sm-2 col-form-label">
+                                    Password
+                                </label>
+                                <div className="col-sm-8">
+                                    <input type="password"
+                                           className="form-control"
+                                           id="password"
+                                           placeholder="123qwe#$%"
+                                           onChange={(e) => this.password = e.target.value}
+                                           required/>
+                                </div>
                             </div>
-                        </div>
 
-                        <div className="form-group row">
-                            <label className="col-sm-2 col-form-label"></label>
-                            <div className="col-sm-8">
-                                <button className="btn btn-primary btn-block"
-                                        type="button"
-                                        id="signInBtn"
-                                        onClick={() => this.login(this.username,this.password)}>
-                                    Sign in
-                                </button>
+                            <div className="form-group row">
+                                <label className="col-sm-2 col-form-label"></label>
+                                <div className="col-sm-8">
+                                    <button className="btn btn-primary btn-block"
+                                            type="button"
+                                            id="signInBtn"
+                                            onClick={() => this.login(this.username, this.password)}>
+                                        Sign in
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </Container>
-            </div>
-        )
+                        </form>
+                    </Container>
+                </div>
+            )
+        }
     }
-}
--- src/components/UserHomeNavbar.js
+++ src/components/UserHomeNavbar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import UserServiceClient from '../services/user.service.client'
 export default class UserHomeNavbar extends Component {
     constructor(props){
         super(props);
