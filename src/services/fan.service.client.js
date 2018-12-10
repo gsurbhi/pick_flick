e19370bb41792f3218cc48a95105914bc775e25e
Surbhi <surbhi910@gmail.com>
2018-12-10 13:31:42 -0500

    Fan service implemented

---
+++ src/services/fan.service.client.js
@@ -0,0 +1,69 @@
+let FAN_URL = 'https://desolate-retreat-56126.herokuapp.com/api/movie';
+
+class FanServiceClient {
+
+    static findAllFans() {
+        return fetch(FAN_URL + 'fan', {
+            method: 'get',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    static followFan(id) {
+        return fetch(FAN_URL + 'fan/' + id, {
+            method: 'post',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+
+    static unfollowFan(item) {
+        return fetch(FAN_URL + 'fan/unfollow', {
+            method: 'delete',
+            credentials: "include",
+            body: JSON.stringify(item),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+
+    static getFanContent(id) {
+        return fetch(FAN_URL + 'fan/likes/' + id, {
+            method: 'get',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+    }
+
+    static getActorContent(id) {
+        return fetch(FAN_URL + 'actor/events/' + id, {
+            method: 'get',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+    }
+
+    static getFollowedFansForUser() {
+        return fetch(FAN_URL + 'fan/following', {
+            method: 'get',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+}
+
+export default FanServiceClient;
